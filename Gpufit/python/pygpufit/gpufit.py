02927452aac783e9ca8604bf47280100412c5f63
Carlo Bevilacqua <carlo.bevilacqua@embl.de>
2025-10-21 11:22:13 +0200

    Added new models

--- Gpufit/python/pygpufit/gpufit.py
+++ Gpufit/python/pygpufit/gpufit.py
@@ -72,6 +72,11 @@ class ModelID:
     SPLINE_3D_PHASE_MULTICHANNEL = 12
     SPLINE_4D = 13
     SPLINE_5D = 14
+    ANTI_STOKES = 15
+    STOKES = 16
+    POLY2 = 17
+    CAUCHY_LORENTZ_1D = 18
+    TIME_DOMAIN_LORENTZ_1D = 19
 
 
 class EstimatorID:
